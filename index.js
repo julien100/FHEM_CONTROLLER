e3c41c0c764d6457acd49c4dc9c9302c357d8a18
Julien Rensch <julienrensch@Juliens-MBP.fritz.box>
2016-12-22 23:42:53 +0100

    fist commit

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 // controller.js
-const mqtt = require('mqtt')
-const client = mqtt.connect('mqtt://localhost')
+const mqtt = require('mqtt');
+const client = mqtt.connect('mqtt://localhost');
+const baseUrl = "localhost:8083";
 
 
 client.on('connect', () => {
   client.subscribe('fhem');
+  console.log("Subscibed to fhem");
 });
 
 client.on('message', (topic, message) => {
   if(topic === 'fhem') {
+    var stringMessage = message.toString();
+    var jsonMessage = JSON.parse(stringMessage);
+    updateState(jsonMessage);
     console.log('fhem message');
   }
 });
+
+function updateState(jsonMessage) {
+  var device = jsonMessage.device;
+  var state = jsonMessage.state;
+  var theUrl = baseUrl + "/fhem?cmd." + device + "=set " + device + " " + state;
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.open("GET", theUrl, true);
+  xmlHttp.send();
+}
