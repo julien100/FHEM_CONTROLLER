3b6efed14e96d0c1200fa4e12e15abeb99ccc6a0
Julien Rensch <julienrensch@Juliens-MBP.fritz.box>
2016-12-23 13:19:30 +0100

    fist commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const client = mqtt.connect('mqtt://localhost');
 const baseUrl = "http://localhost:8083";
 var http = require('http');
 const querystring = require('querystring');
-
+require('./database');
 
 
 client.on('connect', () => {
@@ -14,8 +14,10 @@ client.on('connect', () => {
 
 client.on('message', (topic, message) => {
   if(topic === 'fhem') {
+
     var stringMessage = message.toString();
     var jsonMessage = JSON.parse(stringMessage);
+
     if (jsonMessage.device === 'powernode') {
       updateState({device: "KU.STECKDOSE_1", state: jsonMessage.state});
     } else if (jsonMessage.device === 'house') {
